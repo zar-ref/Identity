85bc7427495a6de8572af4b147b3dbae1c3f4fd7
fif <fif@cdi.ch>
2022-04-19 07:59:43 +0100

    authorize done

--- src/Identity.WebAPI/Controllers/Attributes/AuthorizeAttribute.cs
+++ src/Identity.WebAPI/Controllers/Attributes/AuthorizeAttribute.cs
@@ -5,15 +5,27 @@ namespace Identity.WebAPI.Controllers.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class AuthorizeAttribute : Attribute, IAuthorizationFilter
     {
-        public string [] AllowedRoles { get ; set; }
+        public string [] AllowedRoles { get ; set; } //Should be setted in every controller enpoint
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
             if (allowAnonymous)
                 return;
 
+            var roles = (string[])context.HttpContext.Items["Roles"];
 
-            var ctx = context.HttpContext;
+            bool rolesMatch = true;
+            foreach (var role in roles)
+            {
+                if (!AllowedRoles.Any(_allowedRole => _allowedRole == role))
+                {
+                    rolesMatch = false;
+                    break;
+
+                }
+            }
+            if(!rolesMatch)
+                context.Result = new Microsoft.AspNetCore.Mvc.JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
         }
     }
 }
